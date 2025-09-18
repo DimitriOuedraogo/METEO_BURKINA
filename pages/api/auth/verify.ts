f6838011e91e02fec02b940a51b9fe86183d7313
DimitriOuedraogo <dimitriouedraogo9539@gmail.com>
2025-09-18 13:43:20 +0000

    Corrections verification email

--- pages/api/auth/verify.ts
+++ pages/api/auth/verify.ts
@@ -2,30 +2,32 @@ import prisma from "../../../lib/prisma";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { token, email } = req.query;
+    const { token, email } = req.query;
 
-  if (!token || !email) {
-    return res.status(400).json({ success: false, error: "Lien invalide." });
-  }
+    if (!token || !email) {
+        return res.status(400).json({ success: false, error: "Lien invalide." });
+    }
 
-  const record = await prisma.verificationToken.findUnique({
-    where: { token: String(token) },
-  });
+    const record = await prisma.verificationToken.findUnique({
+        where: { token: decodeURIComponent(String(token)) },
+    });
 
-  if (!record || record.identifier !== email || record.expires < new Date()) {
-    return res.status(400).json({ success: false, error: "Lien expiré ou invalide." });
-  }
 
-  // Vérifier l'utilisateur
-  await prisma.user.update({
-    where: { email: String(email) },
-    data: { emailVerified: new Date() },
-  });
+    const emailDecoded = decodeURIComponent(String(email));
+    if (!record || record.identifier !== emailDecoded || record.expires < new Date()) {
+        return res.status(400).json({ success: false, error: "Lien expiré ou invalide." });
+    }
 
-  // Supprimer le token
-  await prisma.verificationToken.delete({
-    where: { token: String(token) },
-  });
+    // Vérifier l'utilisateur
+    await prisma.user.update({
+        where: { email: String(email) },
+        data: { emailVerified: new Date() },
+    });
 
-  return res.status(200).json({ success: true, message: "Email vérifié avec succès !" });
+    // Supprimer le token
+    await prisma.verificationToken.delete({
+        where: { token: String(token) },
+    });
+
+    return res.status(200).json({ success: true, message: "Email vérifié avec succès !" });
 }
